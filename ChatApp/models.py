85000033efa6102184f0f3de52852fbb38d8b5af
matsumoto <tetsuki0721@gmail.com>
2025-05-23 06:25:42 +0900

    サインアップ処理とログイン処理完成

--- ChatApp/models.py
+++ ChatApp/models.py
@@ -14,12 +14,13 @@ class User:
     def find_user(cls,email):
         # データベース接続プールからコネクションを取得する（DB.pyファイルのこと）
         conn = db_pool.get_conn()
-
+        print(email)
         try:
             with conn.cursor() as cur:
                 sql = "SELECT * FROM users WHERE email=%s;"
                 cur.execute(sql, (email))
                 user = cur.fetchone()   # 設定した辞書型で取得
+                print(user)
             return user
         except pymysql.Error as e:
             print(f'エラーが発生してます：{e}') 
@@ -31,7 +32,17 @@ class User:
     # サインアップ処理
     @classmethod
     def create_user(cls,user_id,company_id,name,email,password):
-        pass
+        conn = db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                sql = "INSERT INTO users (user_id, company_id, user_name, email, pass) VALUES (%s,%s,%s,%s,%s)"
+                cur.execute(sql, (user_id,company_id,name,email,password))
+                conn.commit()
+        except pymysql.Error as e:
+            print(f'エラーが発生してます：{e}') 
+            abort(500)
+        finally:
+            db_pool.release(conn)
 
 
     # 会社情報の取得と登録
@@ -40,15 +51,20 @@ class User:
         conn = db_pool.get_conn()
         try:
             with conn.cursor() as cur:
-                sql = "SELECT company_id FROM companies WHERE name=%s;"
-                cur.execute(sql,(company_name))
+                sql = "SELECT * FROM companies WHERE company_name=%s;"
+                cur.execute(sql, (company_name))
                 company = cur.fetchone()
                 #会社情報があれば返し、無ければ登録して返す
                 if company is None:
-                    sql = "INSERT INTO companies (company_name) VALUES (%s)"
+                    sql = "INSERT INTO companies (company_name) VALUES (%s);"
+                    cur.execute(sql, (company_name))
+                    conn.commit()   #変更をDBに反映
+                    
+                    #登録した会社情報を取得
+                    sql = "SELECT * FROM companies WHERE company_name=%s"
                     cur.execute(sql, (company_name))
                     company = cur.fetchone()
-                return company
+                return company['company_id']
             
         except pymysql.Error as e:
             print(f'エラーが発生してます：{e}') 
